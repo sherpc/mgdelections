4270c1d9bbfc81c342ecd3b5d8b3f312da2930e2
Aleksandr Sher <aleksandrsher@gmail.com>
2014-05-30 21:33:21 +0400

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,43 @@
+function log (data) { console.log(data); }
+
+function autocompleteUrl(term) {
+    return 'http://dom.mos.ru/Lookups/GetAddressAutoComplete?term=' + encodeURIComponent(term);
+}
+
+var availableTags = [
+    "ActionScript",
+    "AppleScript",
+    "Asp",
+    "BASIC",
+    "C",
+    "C++",
+    "Clojure",
+    "COBOL",
+    "ColdFusion",
+    "Erlang",
+    "Fortran",
+    "Groovy",
+    "Haskell",
+    "Java",
+    "JavaScript",
+    "Lisp",
+    "Perl",
+    "PHP",
+    "Python",
+    "Ruby",
+    "Scala",
+    "Scheme"
+];
+
+$(function() {
+    var addressInput = $('.m-address');
+
+    addressInput.autocomplete({
+        source: availableTags
+    });
+
+    /*addressInput.on('input', function () {
+       var term = addressInput.val();
+       log(autocompleteUrl(term));
+    });*/
+});
